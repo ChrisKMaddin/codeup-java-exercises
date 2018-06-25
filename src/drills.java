0a22a6dd11dba79f20e91db9928daa7dbf6ef37d
chriskmaddin <chriskmaddin@gmail.com>
2018-06-24 21:50:37 -0500

    drills

---
+++ src/drills.java
@@ -0,0 +1,14 @@
+//public boolean sleepIn(boolean weekday, boolean vacation) {
+//        if (!weekday || vacation)  {
+//        return true;
+//        }
+//        return false;
+//        }
+//
+//public int diff21(int n) {
+//        if (n <= 21) {
+//        return 21 - n;
+//        } else {
+//        return (n - 21) * 2;
+//        }
+//        }
